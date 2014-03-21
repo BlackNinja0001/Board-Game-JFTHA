ea9b792a90e563428d63a68e0720fb3c90562766
tua84076 <tua84076@CIS-WCC20027.tu.temple.edu>
2014-03-21 17:46:49 -0400

    need to work on the methods specified by comments

---
+++ src/jftha/main/BoardTest.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jftha.main;
+
+public class BoardTest {
+    //At least one of every space
+    //added is method for each spaces
+    //Make it so character would know which spot they landed on
+}
