da8e2774cfe0c2a904e336220325cf817ff602ac
Tue <Tue@Tue-PC.zyxel.com>
2014-03-21 22:07:18 -0400

    Added IterateBoard() and another addSpace() for test purposes. not fully working yet

--- src/jftha/main/Board.java
+++ src/jftha/main/Board.java
@@ -32,6 +32,27 @@ public class Board {
         }
     }
     
+    //addSpace for testing purposes
+    //can leave it or remove later
+    public void addSpace(int inSpaceID, boolean inIsChest, boolean inIsStore, boolean inIsD2D, boolean inIsHealth, boolean inIsCard){
+        Space newSpace = new Space(inSpaceID, inIsChest, inIsStore, inIsD2D, inIsHealth, inIsCard);
+        
+        //If the list is empty, this will create the first Space
+        if(start == end){
+            newSpace.prev = start;
+            newSpace.next = end;
+            start = newSpace;
+            end = newSpace;         
+        }else{
+            Space current = end;
+            
+            newSpace.prev = current;
+            newSpace.next = null;
+            current.next = newSpace;
+            end = newSpace;
+        }
+    }
+    
     public void deleteSpace(int index){
         Space current = start;
         int ret = index;
@@ -54,6 +75,26 @@ public class Board {
         }
         return num;
     }
-    //create double linked list of spaces
-    //create what each spaces is
+    
+    //Test iterator to check if the space location is one of the 5 main spaces
+    //Chest, Store, D2D, Health, or Card, otherwise will just print out the spaceID
+    public void iterateBoard(){
+        Space current = start;
+        while(current.next != null){
+            if(current.getIsCard() == true){
+                System.out.println("This space is a Card Space");
+            }else if(current.getIsChest() == true){
+                System.out.println("This space is a Chest Space");
+            }else if(current.getIsD2D() == true){
+                System.out.println("This space is a D2D Space");
+            }else if(current.getIsHealth() == true){
+                System.out.println("This space is a Health Space");
+            }else if(current.getIsStore() == true){
+                System.out.println("This space is a Store Space");
+            }else{
+                System.out.println("Current Location is " + current.getSpaceID());
+            }
+            current = current.next;
+        }
+    }
 }
