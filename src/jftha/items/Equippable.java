b1179d7342c7c908c65cc5cfcb08092f1d985401
Shane Riggins <tuc33380@temple.edu>
2014-03-24 16:26:18 -0400

    Josh's change to Equippable

--- src/jftha/items/Equippable.java
+++ src/jftha/items/Equippable.java
@@ -1,6 +1,13 @@
 package jftha.items;
 
+import jftha.heroes.*;
+
 public class Equippable extends Item{
     //a Hero can equip only one weapon and one armor <-- should this be in the Hero class?
     //An equippable item affects only the stats of the Hero who has the items equipped
+    private Hero equippedOn;
+    
+    public boolean isEquippedOn(Hero potentialOwner){
+        return (equippedOn == potentialOwner);
+    }
 }
