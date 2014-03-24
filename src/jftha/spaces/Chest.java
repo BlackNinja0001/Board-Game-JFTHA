cdbe46e28e6c66c6927ae11af3a62d74344b66c4
Shane Riggins <tuc33380@temple.edu>
2014-03-24 18:30:36 -0400

    Changed giveItem method to use ItemFactory

--- src/jftha/spaces/Chest.java
+++ src/jftha/spaces/Chest.java
@@ -1,14 +1,22 @@
 package jftha.spaces;
 
 import jftha.heroes.Hero;
+import jftha.items.*;
 
 public class Chest extends Space {
     public Chest() {
         setActivationType('l');
     }
     // Receive an item. The higher the Luck stat, the more likely a rare item will be in the chest.
-    public void giveItem(Hero h) {
-        
-        
+    public void giveItem() {
+        Hero h = getActivator();
+        ItemFactory i = new ItemFactory();
+        if(h.getLuck() > 10) {
+            h.addItem(i.buildItem(RarityEnum.rare));
+        } else if(h.getLuck() > 8) {
+            h.addItem(i.buildItem(RarityEnum.uncommon));
+        } else {
+            h.addItem(i.buildItem(RarityEnum.common));
+        }
     }
 }
