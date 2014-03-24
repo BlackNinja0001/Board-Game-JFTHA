b6c98d9c7d24e40a652013129eb4e861d48696fb
Shane Riggins <tuc33380@temple.edu>
2014-03-24 17:45:36 -0400

    Made skeleton of possible Chest class.  Has giveItem method to generate and give an item to a hero

---
+++ src/jftha/spaces/Chest.java
@@ -0,0 +1,14 @@
+package jftha.spaces;
+
+import jftha.heroes.Hero;
+
+public class Chest extends Space {
+    public Chest() {
+        setActivationType('l');
+    }
+    // Receive an item. The higher the Luck stat, the more likely a rare item will be in the chest.
+    public void giveItem(Hero h) {
+        
+        
+    }
+}
