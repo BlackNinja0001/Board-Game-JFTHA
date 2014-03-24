58c66ea91d0f42e556b7fac5e640b3cfc6f65e5c
Shane Riggins <tuc33380@temple.edu>
2014-03-24 18:32:45 -0400

    Basic itemFactory that so far does nothing.  Should generate a type (rare, uncommon or common) of item.

---
+++ src/jftha/items/ItemFactory.java
@@ -0,0 +1,20 @@
+package jftha.items;
+
+public class ItemFactory {
+    public Item buildItem(RarityEnum rarity) {
+        Item item = null;
+        switch(rarity) {
+            case common:
+                break;
+            case uncommon:
+                break;
+            case rare:
+                break;
+            default:
+                // throw error
+                break;
+        }
+       
+        return item;
+    }
+}
