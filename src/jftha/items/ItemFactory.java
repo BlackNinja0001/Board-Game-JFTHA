90206e592cccbab742caa86a9d8072739db0af46
Shane Riggins <tuc33380@temple.edu>
2014-03-25 08:08:32 -0400

    Revert "Used commonItems to create a random item"
    This reverts commit 52837392452972723fdeb75f59f10e3e9100c121.

--- src/jftha/items/ItemFactory.java
+++ src/jftha/items/ItemFactory.java
@@ -1,25 +1,11 @@
 package jftha.items;
 
-import java.util.Random;
-
 public class ItemFactory {
     public Item buildItem(RarityEnum rarity) {
         Item item = null;
-        Class itemClass;
-        Random rand = new Random();
         switch(rarity) {
             case common:
-                CommonItems ci = new CommonItems();
-                int size = ci.commonClasses.size();
-                int n = rand.nextInt(size);
-                itemClass = ci.commonClasses.get(n);
-                try {
-                    item = (Item)itemClass.newInstance();
-                }catch(InstantiationException ie) {
-                    
-                } catch(IllegalAccessException iae) {
-                    
-                }
+                
                 break;
             case uncommon:
                 break;
