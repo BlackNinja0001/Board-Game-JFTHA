2e1358fb49249c53c174d44617ba9221a92cb108
Shane Riggins <tuc33380@temple.edu>
2014-03-26 10:37:17 -0400

    Added implementation to actually return the correct RarityType item.  Needs Exception handling

--- src/jftha/items/ItemFactory.java
+++ src/jftha/items/ItemFactory.java
@@ -1,15 +1,44 @@
 package jftha.items;
 
+import java.util.Random;
+
 public class ItemFactory {
     public Item buildItem(RarityEnum rarity) {
         Item item = null;
+        ItemLists il = new ItemLists();
+        Random rand = new Random(System.currentTimeMillis());
+        int i, num;
+        Class clazz;
         switch(rarity) {
             case common:
-                
+                i = il.getCommonSize();
+                num = rand.nextInt(i);
+                clazz = il.getCommonClass(num);
+                try {
+                    item = (Item)clazz.newInstance();
+                } catch(IllegalAccessException | InstantiationException ex) {
+                    
+                }
                 break;
             case uncommon:
+                i = il.getUncommonSize();
+                num = rand.nextInt(i);
+                clazz = il.getUncommonClass(num);
+                try {
+                    item = (Item)clazz.newInstance();
+                } catch(IllegalAccessException | InstantiationException ex) {
+                    
+                }
                 break;
             case rare:
+                i = il.getRareSize();
+                num = rand.nextInt(i);
+                clazz = il.getRareClass(num);
+                try {
+                    item = (Item)clazz.newInstance();
+                } catch(IllegalAccessException | InstantiationException ex) {
+                    
+                }
                 break;
             default:
                 // throw error
