1b0d068afe6d484cbcc9f0ef5af81c858657e185
Shane Riggins <tuc33380@temple.edu>
2014-03-26 10:45:32 -0400

    Used Random to improve chances of getting rare item

--- src/jftha/spaces/Chest.java
+++ src/jftha/spaces/Chest.java
@@ -1,5 +1,6 @@
 package jftha.spaces;
 
+import java.util.Random;
 import jftha.heroes.Hero;
 import jftha.items.*;
 
@@ -9,11 +10,13 @@ public class Chest extends Space {
     }
     // Receive an item. The higher the Luck stat, the more likely a rare item will be in the chest.
     public void giveItem() {
+        Random rand = new Random(System.currentTimeMillis());
         Hero h = getActivator();
+        int luck = rand.nextInt(100) + 1 + h.getLuck();
         ItemFactory i = new ItemFactory();
-        if(h.getLuck() > 10) {
+        if(luck > 90) {
             h.addItem(i.buildItem(RarityEnum.rare));
-        } else if(h.getLuck() > 8) {
+        } else if(luck > 75) {
             h.addItem(i.buildItem(RarityEnum.uncommon));
         } else {
             h.addItem(i.buildItem(RarityEnum.common));
