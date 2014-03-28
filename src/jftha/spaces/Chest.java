5dd49f49186d34c9f538a40c3133984606e65cff
Shane Riggins <tuc33380@temple.edu>
2014-03-28 16:39:25 -0400

    Now chest will give back items lost at last death

--- src/jftha/spaces/Chest.java
+++ src/jftha/spaces/Chest.java
@@ -12,14 +12,24 @@ public class Chest extends Space {
     public void giveItem() {
         Random rand = new Random(System.currentTimeMillis());
         Hero h = getActivator();
-        int luck = rand.nextInt(100) + 1 + h.getLuck();
-        ItemFactory i = new ItemFactory();
-        if(luck > 90) {
-            h.addItem(i.buildItem(RarityEnum.rare));
-        } else if(luck > 75) {
-            h.addItem(i.buildItem(RarityEnum.uncommon));
+        // If was resurrected give back lostItems
+        if(h.getWasGhost()) {
+            List<Item> lost = h.getLostItems();
+            for(Item item: lost) {
+                if(!item.getSpiritual())
+                    h.addItem(item);
+                h.setWasGhost(true);
+            }
         } else {
-            h.addItem(i.buildItem(RarityEnum.common));
+            int luck = rand.nextInt(100) + 1 + h.getLuck();
+            ItemFactory i = new ItemFactory();
+            if(luck > 90) {
+                h.addItem(i.buildItem(RarityEnum.rare));
+            } else if(luck > 75) {
+                h.addItem(i.buildItem(RarityEnum.uncommon));
+            } else {
+                h.addItem(i.buildItem(RarityEnum.common));
+            }
         }
     }
 }
