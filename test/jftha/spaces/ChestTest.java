b8881bef3ba481563fc1fc2dc2cdb97e0070c84f
Shane Riggins <tuc33380@temple.edu>
2014-03-28 00:52:52 -0400

    Auto-generated test of Chest

---
+++ test/jftha/spaces/ChestTest.java
@@ -0,0 +1,43 @@
+package jftha.spaces;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ChestTest {
+    
+    public ChestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of giveItem method, of class Chest.
+     */
+    @Test
+    public void testGiveItem() {
+        System.out.println("giveItem");
+        Chest instance = new Chest();
+        instance.giveItem();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
