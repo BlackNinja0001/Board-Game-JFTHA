5805684bb0774fc1d1eae1dccd1225436af0e4e7
tua84076 <tua84076@CIS-WCC20011.tu.temple.edu>
2014-03-28 17:26:22 -0400

    Duel to the Death Class. One of the board space

---
+++ src/jftha/spaces/D2D.java
@@ -0,0 +1,16 @@
+package jftha.spaces;
+
+import java.util.*;
+import jftha.heroes.*;
+import jftha.items.*;
+
+public class D2D extends Space{
+    
+    public D2D(){
+        this.setActivationType('1');
+    }
+    
+    public void duel(Hero beingAttacked){
+        
+    }
+}
