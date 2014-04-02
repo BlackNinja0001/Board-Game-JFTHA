153d33a5f63d14b80750bf9b0a625b408d8185b7
tue75856 <tue75856@CIS-WCC20918.tu.temple.edu>
2014-04-02 11:16:56 -0400

    Health space implementation finished.

--- src/jftha/spaces/Health.java
+++ src/jftha/spaces/Health.java
@@ -3,50 +3,56 @@ package jftha.spaces;
 import java.util.Random;
 import jftha.heroes.Hero;
 
-public class Health {
-    
+public class Health extends Space{
+
     private int statChange;
     private int statChange2; //Sometimes health and magic will be regenerated at the same time
-    
-    public Health(){
+
+    public Health() {
         Random rand = new Random();
         statChange = rand.nextInt(16) + 5; //5-20
         statChange2 = rand.nextInt(16) + 5; //5-20
     }
-    
+
     /**
      * randomizes statChange and statChange2
      */
-    public void randomize(){
+    public void randomize() {
         Random rand = new Random();
         statChange = rand.nextInt(16) + 5; //5-20
         statChange2 = rand.nextInt(16) + 5; //5-20
     }
-    
+
     /**
      * Heals a Hero's health OR magic
-     * 
+     *
      * @param beingHealed the Hero whose stat changes
      * @param stat the stat to be changed; 'h' for health, 'm' for magic
      */
-    public void heal(Hero beingHealed, char stat){
+    public void heal(Hero beingHealed, char stat) {
+        randomize();
         Character c = new Character(stat);
-        if (c.equals(Character.toString('h')) || c.equals(Character.toString('m'))){
-            
+        if (c.equals(Character.toString('h'))) {
+            beingHealed.setCurrentHP(beingHealed.getCurrentHP() + statChange);
+        } else if (c.equals(Character.toString('m'))) {
+            beingHealed.setCurrentMP(beingHealed.getCurrentMP() + statChange);
         }
     }
-    
+
     /**
      * Heals a Hero's health OR magic
-     * 
+     *
      * @param beingHealed the Hero whose stat changes
-     * @param stat the stat to be changed; 'hm' for health and magic; can only be "hm"
+     * @param stat the stat to be changed; 'hm' for health and magic; can only
+     * be "hm"
      */
-    public void heal(Hero beingHealed, String stat){
-        if (stat.equalsIgnoreCase("hm")){
+    public void heal(Hero beingHealed, String stat) {
+        randomize();
+        if (!stat.equalsIgnoreCase("hm")) {
             return; //invalid stat String
         } else {
-            
+            beingHealed.setCurrentHP(beingHealed.getCurrentHP() + statChange);
+            beingHealed.setCurrentMP(beingHealed.getCurrentMP() + statChange2);
         }
     }
 }
