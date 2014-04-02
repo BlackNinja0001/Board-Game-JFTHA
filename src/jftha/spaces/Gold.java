0d36008068655b49147edfb4e9649648a90fdd2d
Shane Riggins <tuc33380@temple.edu>
2014-04-02 18:40:07 -0400

    Added a space that gives players gold

---
+++ src/jftha/spaces/Gold.java
@@ -0,0 +1,33 @@
+package jftha.spaces;
+
+import java.util.Random;
+import jftha.heroes.Hero;
+
+public class Gold {
+    
+    private int amount;
+    
+    public Gold() {
+        Random rand = new Random();
+        amount = rand.nextInt(5) + 1; //1-5
+    }
+
+    /**
+     * randomizes amount and amount2
+     */
+    public void randomize() {
+        Random rand = new Random();
+        amount = rand.nextInt(5) + 1; //1-5
+    }
+
+    /**
+     * Heals a Hero's health OR magic
+     *
+     * @param recipient the Hero whose stat changes
+     */
+    public void giveGold(Hero recipient) {
+        randomize();
+        recipient.setGold(recipient.getGold() + amount);
+        
+    }
+}
