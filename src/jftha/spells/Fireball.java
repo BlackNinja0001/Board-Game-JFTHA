1a099f6bfe76c9cd9ddf8b81647ce6bd81e24491
tua84076 <tua84076@CIS-WCC20919.tu.temple.edu>
2014-04-02 12:30:55 -0400

    Set intial stats

--- src/jftha/spells/Fireball.java
+++ src/jftha/spells/Fireball.java
@@ -3,5 +3,17 @@ package jftha.spells;
 public class Fireball extends Spell {
     public Fireball() {
         this.setGoldCost(10);
+        this.setMinDamage(1);
+        this.setMaxDamage(5);
     }
+    
+    /*
+    public int fireball(Hero caster){
+        double damage;
+        damage = caster.getMagic() + (caster.getLuck() * .2);
+        int intdamage = (int)Math.round(damage);
+        return intdamage;
+    }
+    * 
+    */
 }
