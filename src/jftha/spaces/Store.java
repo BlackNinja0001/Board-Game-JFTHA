fa801a3e0ef41a7b5d06df2179cc6ede1225a390
Shane Riggins <tuc33380@temple.edu>
2014-04-02 14:00:55 -0400

    buy method throws illegal argument exception for default,  buy activated by player

--- src/jftha/spaces/Store.java
+++ src/jftha/spaces/Store.java
@@ -1,6 +1,7 @@
 package jftha.spaces;
 
 import java.util.*;
+import jftha.main.Player;
 import jftha.heroes.*;
 import jftha.items.*;
 
@@ -8,56 +9,6 @@ public class Store extends Space{
     
     public Store(){
         this.setActivationType('p');
-    }
-    
-    //this method is for testing purposes
-    public void buyTest(){
-        Hero h = getActivator();
-
-        ItemFactory i = new ItemFactory();
-        ArrayList<Item> items = new ArrayList<>(5);
-        AnimalSkin as = new AnimalSkin();
-        Ax ax = new Ax();
-        Bible bible = new Bible();
-        SuitOfArmor soa = new SuitOfArmor();
-        Tuxedo tux = new Tuxedo();
-        
-        items.add(as);
-        items.add(ax);
-        items.add(bible);
-        items.add(soa);
-        items.add(tux);
-            
-        for(int j = 0; j < 5; j++){
-            String item = items.get(j).toString();
-            System.out.println("Input " + (j+1) + "to buy " + item);
-        }
-
-        System.out.println("Input 6 to choose not to buy");
-        Scanner scan = new Scanner(System.in);
-        int select = scan.nextInt();
-        switch (select) {
-                case 1:
-                    h.buy(items.get(0));
-                    break;
-                case 2:
-                    h.buy(items.get(1));
-                    break;
-                case 3:
-                    h.buy(items.get(2));
-                    break;
-                case 4:
-                    h.buy(items.get(3));
-                    break;
-                case 5:
-                    h.buy(items.get(4));
-                    break;
-                case 6:
-                    System.out.println("You chose not to buy anything");
-                    break;
-                default:
-                    System.out.println("Error: Did not select an option");
-            }
     }
     
     public void buy(){
@@ -92,34 +43,35 @@ public class Store extends Space{
      */
         
         switch (select) {
-                case 1:
-                    h.buy(items.get(0));
-                    break;
-                case 2:
-                    h.buy(items.get(1));
-                    break;
-                case 3:
-                    h.buy(items.get(2));
-                    break;
-                case 4:
-                    h.buy(items.get(3));
-                    break;
-                case 5:
-                    h.buy(items.get(4));
-                    break;
-                case 6:
-                    System.out.println("You chose not to buy anything");
-                    break;
-                default:
-                    System.out.println("Error: Did not select an option");
+            case 1:
+                h.buy(items.get(0));
+                break;
+            case 2:
+                h.buy(items.get(1));
+                break;
+            case 3:
+                h.buy(items.get(2));
+                break;
+            case 4:
+                h.buy(items.get(3));
+                break;
+            case 5:
+                h.buy(items.get(4));
+                break;
+            case 6:
+                System.out.println("You chose not to buy anything");
+                break;
+            default:
+                throw new IllegalArgumentException("Error: Did not select an option");
             }
     }
     /**
      *
-     * @param hero
+     * @param activator
      */
-    public void buy(Hero hero){
-        hero = getActivator();
+    public void buy(Player activator){
+        this.setActivator(activator.getCharacter());
+        Hero hero = this.getActivator();
         
         Random rand = new Random(System.currentTimeMillis());
         int luck;
@@ -129,42 +81,38 @@ public class Store extends Space{
             luck = rand.nextInt(100) + 1 + hero.getLuck();
             if(luck > 90){
                 items.add(i.buildItem(RarityEnum.rare));
-                String item = items.get(j).toString();
-                System.out.println("Input " + (j+1) + "to buy " + item);
             }else if(luck > 75){
                 items.add(i.buildItem(RarityEnum.uncommon));
-                String item = items.get(j).toString();
-                System.out.println("Input " + (j+1) + "to buy " + item);
             }else{
                 items.add(i.buildItem(RarityEnum.common));
-                String item = items.get(j).toString();
-                System.out.println("Input " + (j+1) + "to buy " + item);
             }
+            String item = items.get(j).toString();
+            System.out.println("Input " + (j+1) + " to buy " + item);
         }
         System.out.println("Input 6 to choose not to buy");
         Scanner scan = new Scanner(System.in);
         int select = scan.nextInt();
-        switch (select) {
-                case 1:
-                    hero.buy(items.get(0));
-                    break;
-                case 2:
-                    hero.buy(items.get(1));
-                    break;
-                case 3:
-                    hero.buy(items.get(2));
-                    break;
-                case 4:
-                    hero.buy(items.get(3));
-                    break;
-                case 5:
-                    hero.buy(items.get(4));
-                    break;
-                case 6:
-                    System.out.println("You chose not to buy anything");
-                    break;
-                default:
-                    System.out.println("Error: Did not select an option");
+    switch (select) {
+            case 1:
+                hero.buy(items.get(0));
+                break;
+            case 2:
+                hero.buy(items.get(1));
+                break;
+            case 3:
+                hero.buy(items.get(2));
+                break;
+            case 4:
+                hero.buy(items.get(3));
+                break;
+            case 5:
+                hero.buy(items.get(4));
+                break;
+            case 6:
+                System.out.println("You chose not to buy anything");
+                break;
+            default:
+                throw new IllegalArgumentException("Error: Did not select an option");
             }
     }
 }
