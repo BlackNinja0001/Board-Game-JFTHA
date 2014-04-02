216e8036adc2d4dfc72c5cf2488f3ffeb8841cdc
tua84076 <tua84076@CIS-WCC20919.tu.temple.edu>
2014-04-02 12:31:07 -0400

    Set intial stats

--- src/jftha/spells/Heal.java
+++ src/jftha/spells/Heal.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package jftha.spells;
 
-/**
- *
- * @author shane
- */
 public class Heal extends Spell{
     
     // Constructor
     public Heal() {
         this.setmpCost(super.getmpCost()); //**Placeholder until real value is set**
+        
+        //This is heal
+        this.setMinDamage(5);
+        this.setMaxDamage(10);
     }
 }
