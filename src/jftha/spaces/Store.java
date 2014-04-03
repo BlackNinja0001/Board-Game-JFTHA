13b3a30d8c873e8f2866a566b6fa30b81323ba0f
Shane Riggins <tuc33380@temple.edu>
2014-04-02 22:29:07 -0400

    As Store generates 5 items it ensures that it doesn't already have it.  A printout is given after an item is selected.

--- src/jftha/spaces/Store.java
+++ src/jftha/spaces/Store.java
@@ -69,44 +69,64 @@ public class Store extends Space{
      *
      * @param activator
      */
-    public void buy(Player activator){
+    public void buy(Player activator) {
         this.setActivator(activator.getCharacter());
         Hero hero = this.getActivator();
         
         Random rand = new Random(System.currentTimeMillis());
         int luck;
-        ItemFactory i = new ItemFactory();
-        ArrayList<Item> items = new ArrayList<>(5);
-        for(int j = 0; j < 5; j++){
+        ItemFactory factory = new ItemFactory();
+        ArrayList<Item> items = new ArrayList<>();
+        
+        while (items.size() < 5){
+            boolean shouldAdd = true;
             luck = rand.nextInt(100) + 1 + hero.getLuck();
+            Item temp;
             if(luck > 90){
-                items.add(i.buildItem(RarityEnum.rare));
-            }else if(luck > 75){
-                items.add(i.buildItem(RarityEnum.uncommon));
-            }else{
-                items.add(i.buildItem(RarityEnum.common));
+                temp = factory.buildItem(RarityEnum.rare);
+            } else if(luck > 75){
+                temp = factory.buildItem(RarityEnum.uncommon);
+            } else{
+                temp = factory.buildItem(RarityEnum.common);
             }
+            for(Item k : items) {
+                if(temp.getClass().equals(k.getClass())) {
+                    shouldAdd = false;
+                }
+            }
+            if(shouldAdd){
+                items.add(temp);
+            }
+        }
+        
+        for (int j = 0; j < 5; j++) {
             String item = items.get(j).toString();
             System.out.println("Input " + (j+1) + " to buy " + item);
         }
+        
         System.out.println("Input 6 to choose not to buy");
         Scanner scan = new Scanner(System.in);
         int select = scan.nextInt();
-    switch (select) {
+        switch (select) {
             case 1:
-                hero.buy(items.get(0));
+                hero.buy(items.get(select-1));
+                System.out.println("You bought a " + items.get(select-1).toString());
                 break;
             case 2:
-                hero.buy(items.get(1));
+                hero.buy(items.get(select-1));
+                System.out.println("You bought a " + items.get(select-1).toString());
                 break;
             case 3:
-                hero.buy(items.get(2));
+                hero.buy(items.get(select-1));
+                System.out.println("You bought a " + items.get(select-1).toString());
                 break;
             case 4:
-                hero.buy(items.get(3));
+                hero.buy(items.get(select-1));
+                System.out.println("You bought a " + items.get(select-1).toString());
                 break;
             case 5:
-                hero.buy(items.get(4));
+                hero.buy(items.get(select-1));
+                System.out.println("You bought a " + items.get(select-1).toString());
                 break;
             case 6:
                 System.out.println("You chose not to buy anything");
