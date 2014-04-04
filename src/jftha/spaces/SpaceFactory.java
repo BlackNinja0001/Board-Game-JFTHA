d969363b79f90597d91f4bde0422b8f22a79cb9b
Shane Riggins <tuc33380@temple.edu>
2014-04-04 18:25:54 -0400

    Returns a type of space based on the SpaceEnum

---
+++ src/jftha/spaces/SpaceFactory.java
@@ -0,0 +1,27 @@
+package jftha.spaces;
+
+public class SpaceFactory {
+    public Space buildItem(SpaceEnum type) {
+        Space space = null;
+        switch(type) {
+            case Chest:
+                space = new Chest();
+                break;
+            case D2D:
+                space = new D2D();
+                break;
+            case Gold:
+                space = new Gold();
+                break;
+            case Health:
+                space = new Health();
+                break;
+            case Store:
+                space = new Store();
+                break;
+            default:
+                throw new IllegalArgumentException("Oops. There is no such type of space.");
+        }
+        return space;
+    }
+}
