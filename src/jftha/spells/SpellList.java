2a5c4009b0d04bc3b283417fcc34f8cb513fe309
Shane Riggins <tuc33380@temple.edu>
2014-04-04 16:55:20 -0400

    Made getters for size of spellList and to return a class in spellList

--- src/jftha/spells/SpellList.java
+++ src/jftha/spells/SpellList.java
@@ -5,6 +5,14 @@ import java.util.List;
 
 public class SpellList {
     private List<Class> spellList = new ArrayList<>();
+    
+    public int getSpellSize() {
+        return spellList.size();
+    }
+    public Class getSpellClass(int i) {
+        return spellList.get(i);
+    }
+    
     public SpellList() {
         spellList.add(Fireball.class);
         spellList.add(Heal.class);
