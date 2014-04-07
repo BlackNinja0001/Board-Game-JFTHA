59735ca8067d9ad2ff2d2749e511843593d1a787
Shane Riggins <tuc33380@temple.edu>
2014-04-07 17:53:24 -0400

    Only one bank constructor

--- src/jftha/spaces/SpaceFactory.java
+++ src/jftha/spaces/SpaceFactory.java
@@ -4,9 +4,6 @@ public class SpaceFactory {
     public Space buildItem(SpaceEnum type) {
         Space space = null;
         switch(type) {
-            case Bank:
-                space = new Bank();
-                break;
             case Chest:
                 space = new Chest();
                 break;
