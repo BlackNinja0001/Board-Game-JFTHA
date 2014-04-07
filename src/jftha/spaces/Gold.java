779a9121522cc8e4954574392a5f1366a38e19a6
Shane Riggins <tuc33380@temple.edu>
2014-04-06 21:35:57 -0400

    Renamed giveGold to triggerEffect

--- src/jftha/spaces/Gold.java
+++ src/jftha/spaces/Gold.java
@@ -10,12 +10,10 @@ public class Gold extends Space {
     public Gold() {
         this.setActivationType('p');
         this.setSpaceType(SpaceEnum.Gold);
-        Random rand = new Random();
-        amount = rand.nextInt(5) + 1; //1-5
     }
 
     /**
-     * randomizes amount and amount2
+     * randomizes amount
      */
     public void randomize() {
         Random rand = new Random();
@@ -23,12 +21,13 @@ public class Gold extends Space {
     }
 
     /**
-     * Heals a Hero's health OR magic
+     * Gives the hero an amount of gold between 1 and 5.
      *
-     * @param recipient the Hero whose stat changes
      */
-    public void giveGold(Hero recipient) {
+    @Override
+    public void triggerEffect() {
         randomize();
+        Hero recipient = this.getActivator();
         recipient.setGold(recipient.getGold() + amount);
         
     }
