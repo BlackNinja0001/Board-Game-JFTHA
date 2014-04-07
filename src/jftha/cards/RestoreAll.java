d74d43d11e7fbf28758f9a66b9c6739d0cc509f1
Tue <Tue@Tue-PC.zyxel.com>
2014-04-07 12:56:20 -0400

    Added: Restore all HP/MP if not ghost. Else, will only restore mp

---
+++ src/jftha/cards/RestoreAll.java
@@ -0,0 +1,34 @@
+package jftha.cards;
+
+import jftha.heroes.*;
+import jftha.main.*;
+
+public class RestoreAll extends Card{
+
+    @Override
+    public void triggerEffect() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected) {
+        Hero hero = affected.getCharacter();
+        if(hero.isGhost() == false){
+            hero.setCurrentHP(hero.getMaxHP());
+            hero.setCurrentMP(hero.getMaxMP());
+        }else if(hero.isGhost() == true){
+            hero.setCurrentMP(hero.getMaxMP());
+        }
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2, Player affected3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
