c708f7e258d8a978c617d5388685824476637267
Tue <Tue@Tue-PC.zyxel.com>
2014-04-07 12:57:05 -0400

    Added: Restore MP

---
+++ src/jftha/cards/RestoreMP.java
@@ -0,0 +1,29 @@
+package jftha.cards;
+
+import jftha.heroes.*;
+import jftha.main.*;
+
+public class RestoreMP extends Card{
+
+    @Override
+    public void triggerEffect() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected) {
+        Hero hero = affected.getCharacter();
+        hero.setCurrentMP(hero.getMaxMP());
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2, Player affected3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
