36ea3002aa7c1623dadb3ce5b6b414e6e0e58097
Tue <Tue@Tue-PC.zyxel.com>
2014-04-07 12:55:09 -0400

    Added: Gives away free common item

---
+++ src/jftha/cards/FreeCommonItem.java
@@ -0,0 +1,32 @@
+package jftha.cards;
+
+import java.util.*;
+import jftha.heroes.*;
+import jftha.items.*;
+import jftha.main.*;
+
+public class FreeCommonItem extends Card{
+
+    @Override
+    public void triggerEffect() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected) {
+        Hero hero = affected.getCharacter();
+        ItemFactory i = new ItemFactory();
+        hero.addItem(i.buildItem(jftha.items.RarityEnum.common));   
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2, Player affected3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
