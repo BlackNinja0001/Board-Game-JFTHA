444588c7fd0c7cc1cc22b6fa0d2e46bae48416fa
Shane Riggins <tuc33380@temple.edu>
2014-04-07 17:49:21 -0400

    Added triggerEffect(Hero) support //Exception not supported

--- src/jftha/spaces/Chest.java
+++ src/jftha/spaces/Chest.java
@@ -47,4 +47,9 @@ public class Chest extends Space {
         }
         return luck;
     }
+
+    @Override
+    public void triggerEffect(Hero affected) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
