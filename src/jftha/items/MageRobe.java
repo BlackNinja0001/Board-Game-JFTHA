c209136fd2e73a5b58fb92790afc323a4472682f
Tue <Tue@Tue-PC.zyxel.com>
2014-04-09 00:40:00 -0400

    Edit: MageRobe

--- src/jftha/items/MageRobe.java
+++ src/jftha/items/MageRobe.java
@@ -1,7 +1,38 @@
 package jftha.items;
 
-public class MageRobe extends Item {
+import jftha.heroes.Hero;
+import jftha.main.Player;
+
+public class MageRobe extends Armor {
+    private final int magic = 2;
+    private final int mp = 5;
+    
     public MageRobe() {
         this.setRarity(RarityEnum.common);
+        this.setMessage("Mage Robe: +" + magic + " Magic, " + "+" + mp + " MP");
+    }
+
+    @Override
+    public void equipArmor(Player player) {
+        Hero hero = player.getCharacter();
+        hero.setMagic(hero.getMagic() + magic);
+        hero.setMaxMP(hero.getMaxMP() + mp);
+    }
+
+    @Override
+    public void dropArmor(Player player) {
+        Hero hero = player.getCharacter();
+        hero.setMagic(hero.getMagic() - magic);
+        hero.setMaxMP(hero.getMaxMP() - mp);
+    }
+
+    @Override
+    public void armorEffect(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void armorEffect(Player attacker, Player attacked) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
