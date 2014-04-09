91bf1db606a57d7e5ae6fb8730da81dc8655ee5f
tua84076 <tua84076@CIS-WCC20919.tu.temple.edu>
2014-04-09 11:58:47 -0400

    Edit: Implemented abstract methods

--- src/jftha/items/Bible.java
+++ src/jftha/items/Bible.java
@@ -1,8 +1,30 @@
 package jftha.items;
 
+import jftha.main.Player;
+
 public class Bible extends Item {
     public Bible() {
         this.setSpiritual(true);
         this.setRarity(RarityEnum.common);
+    }
+
+    @Override
+    public void triggerEffect() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2, Player affected3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
--- src/jftha/items/Cloak.java
+++ src/jftha/items/Cloak.java
@@ -1,7 +1,35 @@
 package jftha.items;
 
-public class Cloak extends Item {
+import jftha.heroes.Hero;
+import jftha.main.Player;
+
+public class Cloak extends Armor {
+    private final int agility = 2;
+    
     public Cloak() {
         this.setRarity(RarityEnum.common);
+        this.setMessage("Dagger: +" + agility + " Agility");
+    }
+
+    @Override
+    public void equipArmor(Player player) {
+        Hero hero = player.getCharacter();
+        hero.setAgility(hero.getAgility() + agility);
+    }
+
+    @Override
+    public void dropArmor(Player player) {
+        Hero hero = player.getCharacter();
+        hero.setAgility(hero.getAgility() - agility);
+    }
+
+    @Override
+    public void armorEffect(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void armorEffect(Player attacker, Player attacked) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
