65c8d2f4bcf68ecaf0cad0a61a4b66ecddb68dd9
Tue <Tue@Tue-PC.zyxel.com>
2014-04-09 00:40:41 -0400

    Edit: changed rarity to "uncommon". added stats

--- src/jftha/items/ShiningArmor.java
+++ src/jftha/items/ShiningArmor.java
@@ -1,7 +1,38 @@
 package jftha.items;
 
+import jftha.heroes.Hero;
+import jftha.main.Player;
+
 public class ShiningArmor extends Armor {
+    private final int hp = 20;
+    private final int def = 5;
+    
     public ShiningArmor() {
-        this.setRarity(RarityEnum.common);
+        this.setRarity(RarityEnum.uncommon);
+        this.setMessage("Shining Armor: +" + hp + " HP, " + "+" + def + " Defense");
+    }
+
+    @Override
+    public void equipArmor(Player player) {
+        Hero hero = player.getCharacter();
+        hero.setMaxHP(hero.getMaxHP() + hp);
+        hero.setDefense(hero.getDefense() + def);
+    }
+
+    @Override
+    public void dropArmor(Player player) {
+        Hero hero = player.getCharacter();
+        hero.setMaxHP(hero.getMaxHP() - hp);
+        hero.setDefense(hero.getDefense() - def);
+    }
+
+    @Override
+    public void armorEffect(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void armorEffect(Player attacker, Player attacked) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
