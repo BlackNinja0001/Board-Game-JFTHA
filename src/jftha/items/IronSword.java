29e88536f229f319eabda80a00ef561e5252d6a6
Tue <Tue@Tue-PC.zyxel.com>
2014-04-09 00:38:47 -0400

    Edit: edited IronSword

--- src/jftha/items/IronSword.java
+++ src/jftha/items/IronSword.java
@@ -1,20 +1,27 @@
 package jftha.items;
 
+import jftha.heroes.Hero;
 import jftha.main.Player;
 
 public class IronSword extends Weapon {
+    
+    private final int strength = 2;
+    
     public IronSword() {
         this.setRarity(RarityEnum.common);
+        this.setMessage("Iron Sword: +" + strength + " Strength");
     }
 
     @Override
     public void equipWeap(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Hero hero = player.getCharacter();
+        hero.setStrength(hero.getStrength() + strength);
     }
 
     @Override
     public void dropWeap(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Hero hero = player.getCharacter();
+        hero.setStrength(hero.getStrength() - strength);
     }
 
     @Override
