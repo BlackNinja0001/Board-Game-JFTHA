61ca2b8cdb1fb96423d7c9e70c81a26a5064f27a
Tue <Tue@Tue-PC.zyxel.com>
2014-04-09 00:41:38 -0400

    Edit:

--- src/jftha/items/Equippable.java
+++ src/jftha/items/Equippable.java
@@ -6,17 +6,6 @@ public class Equippable extends Item{
     //a Hero can equip only one weapon and one armor <-- should this be in the Hero class?
     //An equippable item affects only the stats of the Hero who has the items equipped
     private Hero equippedOn;
-    private String message;
-
-    //Getter Methods
-    public String getMessage() {
-        return message;
-    }
-
-    //Setter Methods
-    public void setMessage(String message) {
-        this.message = message;
-    }
     
     public boolean isEquippedOn(Hero potentialOwner){
         return (equippedOn == potentialOwner);
