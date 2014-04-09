6f4a041e8558aa74c7aa1a233efbd89685f280e9
tua84076 <tua84076@CIS-WCC20919.tu.temple.edu>
2014-04-09 11:55:44 -0400

    Edit: Implemented class

--- src/jftha/items/AnimalSkin.java
+++ src/jftha/items/AnimalSkin.java
@@ -1,7 +1,36 @@
 package jftha.items;
 
-public class AnimalSkin extends Item {
+import jftha.heroes.Hero;
+import jftha.main.Player;
+
+public class AnimalSkin extends Armor {
+    private final int def = 2;
+    
     public AnimalSkin() {
         this.setRarity(RarityEnum.common);
+        this.setMessage("Animal Skin: +" + def + " Defense");
+    }
+
+
+    @Override
+    public void equipArmor(Player player) {
+        Hero hero = player.getCharacter();
+        hero.setDefense(hero.getDefense() + def);
+    }
+
+    @Override
+    public void dropArmor(Player player) {
+        Hero hero = player.getCharacter();
+        hero.setDefense(hero.getDefense() - def);
+    }
+
+    @Override
+    public void armorEffect(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void armorEffect(Player attacker, Player attacked) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
