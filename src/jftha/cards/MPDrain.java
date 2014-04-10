d44d5f3f81c84d98c364c6b8f6ff683c5b3e0038
Shane Riggins <tuc33380@temple.edu>
2014-04-10 11:23:21 -0400

    Added NO EFFECT comments

--- src/jftha/cards/MPDrain.java
+++ src/jftha/cards/MPDrain.java
@@ -5,10 +5,10 @@ import jftha.heroes.*;
 import jftha.main.Player;
 
 public class MPDrain extends Card{
-    //This effect can have multiple Constructors
-    //MPDrain can have one player steal MP from another player
-    //MPDrain can drain the current player's MP
     
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -55,6 +55,9 @@ public class MPDrain extends Card{
         }
     }
 
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect(Player affected1, Player affected2, Player affected3) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
