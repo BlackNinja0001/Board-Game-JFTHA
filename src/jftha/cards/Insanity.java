162677eab5baf5132c1fa02880f7b4acfe1da8c6
tue75856 <tue75856@CIS-WCC20005.tu.temple.edu>
2014-04-11 16:43:02 -0400

    Added Insanity Card.

---
+++ src/jftha/cards/Insanity.java
@@ -0,0 +1,29 @@
+package jftha.cards;
+
+import jftha.heroes.Hero;
+import jftha.main.Player;
+
+public class Insanity extends Card{
+
+    @Override
+    public void triggerEffect() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected) {
+        
+        this.setMessage("You thought that you could fly and jumped out a 2-story window. Lose 2 turns for physical and mental therapy.");
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void triggerEffect(Player affected1, Player affected2, Player affected3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
