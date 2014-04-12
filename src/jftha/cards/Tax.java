ab80a09a7c7beb2457d3ed70b7f6a6550df86de2
Shane Riggins <tuc33380@temple.edu>
2014-04-12 19:35:42 -0400

    Tax - Added NO EFFECT comments

--- src/jftha/cards/Tax.java
+++ src/jftha/cards/Tax.java
@@ -5,7 +5,10 @@ import jftha.main.*;
 import jftha.spaces.*;
 
 public class Tax extends Card{
-
+    
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -19,16 +22,24 @@ public class Tax extends Card{
          this.setMessage("Today is a good day for medieval capitalism. Pay " + tax + " gold to the nobles.");
     }
 
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect(Player affected) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect(Player affected1, Player affected2) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect(Player affected1, Player affected2, Player affected3) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
