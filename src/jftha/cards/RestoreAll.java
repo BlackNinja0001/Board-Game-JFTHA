48257af5faeabbf36cb97cf2f6225a88e2cfa723
Shane Riggins <tuc33380@temple.edu>
2014-04-12 19:34:07 -0400

    RestoreAll - Added NO EFFECT comments

--- src/jftha/cards/RestoreAll.java
+++ src/jftha/cards/RestoreAll.java
@@ -5,6 +5,9 @@ import jftha.main.*;
 
 public class RestoreAll extends Card{
 
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -16,17 +19,24 @@ public class RestoreAll extends Card{
         if(hero.isGhost() == false){
             hero.setCurrentHP(hero.getMaxHP());
             hero.setCurrentMP(hero.getMaxMP());
+            this.setMessage("The Almighty One loves you. Restore HP and MP to max.");
         }else if(hero.isGhost() == true){
             hero.setCurrentMP(hero.getMaxMP());
+            this.setMessage("The Almighty One loves you. Restore MP to max.");
         }
-        this.setMessage("The Almighty One loves you. Restore HP and MP to max.");
     }
 
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect(Player affected1, Player affected2) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**NO EFFECT.
+     * 
+     */
     @Override
     public void triggerEffect(Player affected1, Player affected2, Player affected3) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
