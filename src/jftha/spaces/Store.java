ccee08199c0e80979cd16fcc9310c1b626cf66a9
tue75856 <tue75856@CIS-WCC20919.tu.temple.edu>
2014-04-16 12:28:45 -0400

    Print statements buying at store fixed.

--- src/jftha/spaces/Store.java
+++ src/jftha/spaces/Store.java
@@ -5,13 +5,13 @@ import jftha.main.Player;
 import jftha.heroes.*;
 import jftha.items.*;
 
-public class Store extends Space{
-    
-    public Store(){
+public class Store extends Space {
+
+    public Store() {
         this.setActivationType('p');
         this.setSpaceType(SpaceEnum.Store);
     }
-        
+
     /**
      *
      */
@@ -19,73 +19,54 @@ public class Store extends Space{
     public void triggerEffect() {
         Hero hero = this.getActivator();
         System.out.println("You have " + hero.getGold() + " gold");
-        
+
         Random rand = new Random(System.currentTimeMillis());
         int luck;
         ItemFactory factory = new ItemFactory();
         ArrayList<Item> items = new ArrayList<>();
-        
-        while (items.size() < 5){
+
+        while (items.size() < 5) {
             boolean shouldAdd = true;
             luck = rand.nextInt(100) + 1 + hero.getLuck();
             Item temp;
-            if(luck > 90){
+            if (luck > 90) {
                 temp = factory.buildItem(RarityEnum.rare);
-            } else if(luck > 75){
+            } else if (luck > 75) {
                 temp = factory.buildItem(RarityEnum.uncommon);
-            } else{
+            } else {
                 temp = factory.buildItem(RarityEnum.common);
             }
-            for(Item k : items) {
-                if(temp.getClass().equals(k.getClass())) {
+            for (Item k : items) {
+                if (temp.getClass().equals(k.getClass())) {
                     shouldAdd = false;
                 }
             }
-            if(shouldAdd){
+            if (shouldAdd) {
                 items.add(temp);
             }
         }
-        
+
         for (int j = 0; j < 5; j++) {
             String item = items.get(j).toString();
-            System.out.println("Input " + (j+1) + " to buy " + item + ". Cost " + items.get(j).getGoldCost() + " gold");
+            System.out.println("Input " + (j + 1) + " to buy " + item + ". Cost " + items.get(j).getGoldCost() + " gold");
         }
-        
+
         System.out.println("Input 6 to choose not to buy");
         Scanner scan = new Scanner(System.in);
         int select = scan.nextInt();
-        switch (select) {
-            case 1:
-                hero.buy(items.get(select-1));
-                System.out.println("You bought a " + items.get(select-1).toString());
+        boolean crap;
+
+        if ((select >= 1) && (select < 6)) {
+            crap = hero.buy(items.get(select - 1));
+            if (crap) {
+                System.out.println("You bought a " + items.get(select - 1).toString());
                 System.out.println("You have " + hero.getGold() + " gold left");
-                break;
-            case 2:
-                hero.buy(items.get(select-1));
-                System.out.println("You bought a " + items.get(select-1).toString());
-                System.out.println("You have " + hero.getGold() + " gold left");
-                break;
-            case 3:
-                hero.buy(items.get(select-1));
-                System.out.println("You bought a " + items.get(select-1).toString());
-                System.out.println("You have " + hero.getGold() + " gold left");
-                break;
-            case 4:
-                hero.buy(items.get(select-1));
-                System.out.println("You bought a " + items.get(select-1).toString());
-                System.out.println("You have " + hero.getGold() + " gold left");
-                break;
-            case 5:
-                hero.buy(items.get(select-1));
-                System.out.println("You bought a " + items.get(select-1).toString());
-                System.out.println("You have " + hero.getGold() + " gold left");
-                break;
-            case 6:
-                System.out.println("You chose not to buy anything");
-                break;
-            default:
-                throw new IllegalArgumentException("Error: Did not select an option");
             }
+        } else if (select == 6) {
+            System.out.println("You chose not to buy anything");
+        } else {
+            throw new IllegalArgumentException("Error: Did not select an option");
+        }
     }
 
     @Override
