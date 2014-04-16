88ed222e19e1c00e77609b667dddb8f8146f0fed
Shane Riggins <tuc33380@temple.edu>
2014-04-16 11:31:07 -0400

    Store - removed buy method

--- src/jftha/spaces/Store.java
+++ src/jftha/spaces/Store.java
@@ -11,66 +11,7 @@ public class Store extends Space{
         this.setActivationType('p');
         this.setSpaceType(SpaceEnum.Store);
     }
-    
-    public void buy(){
-        Hero h = getActivator();
-        System.out.println("You have " + h.getGold() + " gold");
-        Random rand = new Random(System.currentTimeMillis());
-        int luck;
-        ItemFactory i = new ItemFactory();
-        ArrayList<Item> items = new ArrayList<>(5);
-        for(int j = 0; j < 5; j++){
-            luck = rand.nextInt(100) + 1 + h.getLuck();
-            if(luck > 90){
-                items.add(i.buildItem(RarityEnum.rare));
-                String item = items.get(j).toString();
-                System.out.println("Input " + (j+1) + "to buy " + item + ". Cost " + items.get(j).getGoldCost() + " gold");
-            }else if(luck > 75){
-                items.add(i.buildItem(RarityEnum.uncommon));
-                String item = items.get(j).toString();
-                System.out.println("Input " + (j+1) + "to buy " + item + ". Cost " + items.get(j).getGoldCost() + " gold");
-            }else{
-                items.add(i.buildItem(RarityEnum.common));
-                String item = items.get(j).toString();
-                System.out.println("Input " + (j+1) + "to buy " + item + ". Cost " + items.get(j).getGoldCost() + " gold");
-            }
-        }
-        System.out.println("Input 6 to choose not to buy");
-        Scanner scan = new Scanner(System.in);
-        int select = scan.nextInt();
-    
-    /*Add in if hero's items/spells are full ask which one to replace and use
-     * replaceItem/replaceSpell in Hero
-     */
         
-        switch (select) {
-            case 1:
-                h.buy(items.get(0));
-                System.out.println("You have " + h.getGold() + " gold left");
-                break;
-            case 2:
-                h.buy(items.get(1));
-                System.out.println("You have " + h.getGold() + " gold left");
-                break;
-            case 3:
-                h.buy(items.get(2));
-                System.out.println("You have " + h.getGold() + " gold left");
-                break;
-            case 4:
-                h.buy(items.get(3));
-                System.out.println("You have " + h.getGold() + " gold left");
-                break;
-            case 5:
-                h.buy(items.get(4));
-                System.out.println("You have " + h.getGold() + " gold left");
-                break;
-            case 6:
-                System.out.println("You chose not to buy anything");
-                break;
-            default:
-                throw new IllegalArgumentException("Error: Did not select an option");
-            }
-    }
     /**
      *
      */
