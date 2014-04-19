83b7639136b54929076b6efcc7afdafd1459aa3e
Shane Riggins <tuc33380@temple.edu>
2014-04-18 23:05:22 -0400

    Monster - made a monster that extends hero. Used in Monster spaces

---
+++ src/jftha/heroes/Monster.java
@@ -0,0 +1,17 @@
+package jftha.heroes;
+
+public class Monster extends Hero{
+    
+    public Monster() {
+        setMaxHP(5);
+        setCurrentHP(5);
+        setMaxMP(2);
+        setCurrentMP(2);
+        setStrength(2);
+        setAgility(1);
+        setMagic(2);
+        setLuck(2);
+        setDefense(5);
+    }
+    
+}
