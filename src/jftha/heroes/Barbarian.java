95c8a50d2e8f831a534dedeccce019cb4116480d
Shane Riggins <tuc33380@temple.edu>
2014-04-18 23:04:47 -0400

    Barbarian - Simplified attack

--- src/jftha/heroes/Barbarian.java
+++ src/jftha/heroes/Barbarian.java
@@ -22,40 +22,14 @@ public class Barbarian extends Hero {
         int strength = this.getStrength();
         Random rand = new Random();
         if (getWasAttacked()) { //Barbarian's special
+            int tempStrength = this.getStrength();
             if ((rand.nextInt(5) + 1) == 1) {
-                strength *= 2;
+                tempStrength *= 2;
+                this.setStrength(tempStrength);
             }
         }
-        int randomDamage = rand.nextInt(3);
-        double damage = (strength - enemy.getDefense()) - (0.2 * (this.getLuck() - enemy.getLuck())) + randomDamage;
-        if (damage < 0) { //attacker sucks
-            damage = 0;
-        }
-        int intDamage = (int) Math.round(damage);
-        if (enemy.getWasAttacked() == false) { // **?? Can only get attacked once per turn?
-            if (enemy.isGhost() == false) { //cannot attack ghost unless under certain circumstances
-                enemy.setCurrentHP(enemy.getCurrentHP() - intDamage);
-                if (enemy.getCurrentHP() <= 0) {
-                    enemy.makeGhost();
-                }
-            } else { //attacking ghost
-                //handle spiritual items
-                if (this.getItems().isEmpty()) {
-                    for (Item i : this.getItems()) {
-                        if (i instanceof Equippable) {
-                            Equippable eq = (Equippable) i; //cannot use isEquippedOn() right away, must downcast to child class
-                            if (eq.isEquippedOn(this) && (eq.getSpiritual())) {
-                                enemy.setCurrentMP(enemy.getCurrentMP() - intDamage);
-                                if (enemy.getCurrentMP() <= 0) {
-                                    enemy.setEliminated(true);
-                                }
-                            }
-                        }
-                    }
-                }
-                //if no spiritual items, the Attack phase is skipped
-            }
-        }
-        enemy.setWasAttacked(true);
+        super.attackEnemy(enemy);
+        this.setStrength(strength);
+        
     }
 }
