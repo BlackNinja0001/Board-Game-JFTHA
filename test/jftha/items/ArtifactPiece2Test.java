315c1c78c21c9f4dd34328c71e0261f3d774b515
Shane Riggins <tuc33380@temple.edu>
2014-04-22 18:15:03 -0400

    ArtifactPiece Tests - Wrote passing tests

---
+++ test/jftha/items/ArtifactPiece2Test.java
@@ -0,0 +1,58 @@
+package jftha.items;
+
+import jftha.heroes.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ArtifactPiece2Test {
+    ArtifactPiece2 part1, part2;
+    Hero hero;
+    
+    public ArtifactPiece2Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        part1 = ArtifactPiece2.getInstance();
+        part2 = ArtifactPiece2.getInstance();
+        hero = new Knight();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testUnique() {
+        assertEquals(part1, part2);
+    }
+    
+    @Test
+    public void testProperties() {
+        assertEquals(500, part1.getGoldCost());
+        assertEquals(RarityEnum.rare, part1.getRarity());
+        assertNull(part1.getOwner());
+    }
+    
+    @Test
+    public void testOwnerChange() {
+        hero.addItem(part1);
+        hero.addItem(part2);
+        assertEquals(hero, part1.getOwner());
+        assertEquals(hero, part2.getOwner());
+    }
+    
+    
+}
---
+++ test/jftha/items/ArtifactPiece3Test.java
@@ -0,0 +1,56 @@
+package jftha.items;
+
+import jftha.heroes.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ArtifactPiece3Test {
+    ArtifactPiece3 part1, part2;
+    Hero hero;
+    
+    public ArtifactPiece3Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        part1 = ArtifactPiece3.getInstance();
+        part2 = ArtifactPiece3.getInstance();
+        hero = new Knight();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testUnique() {
+        assertEquals(part1, part2);
+    }
+    
+    @Test
+    public void testProperties() {
+        assertEquals(500, part1.getGoldCost());
+        assertEquals(RarityEnum.rare, part1.getRarity());
+        assertNull(part1.getOwner());
+    }
+    
+    @Test
+    public void testOwnerChange() {
+        hero.addItem(part1);
+        hero.addItem(part2);
+        assertEquals(hero, part1.getOwner());
+        assertEquals(hero, part2.getOwner());
+    }
+}
