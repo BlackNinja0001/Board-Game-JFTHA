dccff63003d67a4374a9ea72f13a1e36e3b30ebd
Shane Riggins <tuc33380@temple.edu>
2014-04-26 17:18:06 -0400

    Equipped items handled in hero class

--- src/jftha/items/Equippable.java
+++ src/jftha/items/Equippable.java
@@ -4,14 +4,7 @@ import jftha.heroes.*;
 import jftha.main.Player;
 
 public class Equippable extends Item{
-    //a Hero can equip only one weapon and one armor <-- should this be in the Hero class?
-    //An equippable item affects only the stats of the Hero who has the items equipped
-    private Hero equippedOn;
-    
-    public boolean isEquippedOn(Hero potentialOwner){
-        return (equippedOn == potentialOwner);
-    }
-
+   
     @Override
     public void triggerEffect() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
