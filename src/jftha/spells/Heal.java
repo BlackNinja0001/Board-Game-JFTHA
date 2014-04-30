fe762a4bb8489aa32de961957db18e18c708ca63
Shane Riggins <tuc33380@temple.edu>
2014-04-30 10:54:34 -0400

    Heal - change is in correct range

--- src/jftha/spells/Heal.java
+++ src/jftha/spells/Heal.java
@@ -1,8 +1,6 @@
-package jftha.spells;
+ package jftha.spells;
 
-import jftha.heroes.Hero;
-import jftha.heroes.Mage;
-import jftha.statchanges.CurHP_SCPT;
+import jftha.heroes.*;
 
 public class Heal extends Spell{
     
@@ -14,34 +12,31 @@ public class Heal extends Spell{
         this.setmpCost(5); //**Placeholder until real value is set**
         
         //This is heal
-        this.setMinDamage(5);
-        this.setMaxDamage(10);
+        this.setMinHPChange(5);
+        this.setMaxHPChange(10);
         
-        this.setMessage("Heal. Heals current player between 5-10 HP. Cost " + this.getmpCost() + "MP");
+        this.setMessage("Heal. Heals current player between 5-10 HP. Cost " + this.getmpCost() + " MP");
     }
 
     @Override
     public void castSpell(Hero caster) {
-        //Damage is the heal amount
-        if (getCurrentCD() > 0) {
-            setCurrentCD(this.getMaxCooldown());
-            int spellEffect = getHpChange();
-            double actualEffect = ((caster.getMagic() * .5) + spellEffect + (caster.getLuck() * .2));
-            int finalEffect = (int) Math.round(actualEffect);
-            if(finalEffect <=0) {
-                finalEffect = 1;
-            }
-            
-            if(caster instanceof Mage) {
-                caster.setCurrentMP(caster.getCurrentMP() - (int)(getmpCost() * Mage.multiplier));
-            } else {
-                caster.setCurrentMP(caster.getCurrentMP() - getmpCost());
-            }
-            
-            caster.addTSC(new CurHP_SCPT(1, finalEffect));
-        } else  {
-            setCurrentCD(this.getCurrentCD() - 1);
+        setCurrentCD(this.getMaxCooldown());
+        int spellEffect = getHpChange();
+        double actualEffect = ((caster.getMagic() * .5) + spellEffect + (caster.getLuck() * .2));
+        int finalEffect = (int) Math.round(actualEffect);
+        if(finalEffect < this.getMinHPChange()) {
+            finalEffect = this.getMinHPChange();
         }
+        if(finalEffect > this.getMaxHPChange()) {
+            finalEffect = this.getMaxHPChange();
+        }
+        if(caster instanceof Mage) {
+            caster.setCurrentMP(caster.getCurrentMP() - (int)(getmpCost() * Mage.multiplier));
+        } else {
+            caster.setCurrentMP(caster.getCurrentMP() - getmpCost());
+        }
+
+        caster.setCurrentHP(caster.getCurrentHP() + finalEffect);
     }
 
     @Override
