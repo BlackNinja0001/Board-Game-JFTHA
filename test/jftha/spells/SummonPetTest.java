1c75266ade48e3ee532a43175db9731f35e1736d
Shane Riggins <tuc33380@temple.edu>
2014-04-30 11:43:17 -0400

    SummonPetTest - three working tests

--- test/jftha/spells/SummonPetTest.java
+++ test/jftha/spells/SummonPetTest.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package jftha.spells;
 
-import jftha.heroes.Hero;
+import jftha.heroes.*;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,10 +8,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author shane
- */
 public class SummonPetTest {
     
     public SummonPetTest() {
@@ -40,80 +30,29 @@ public class SummonPetTest {
     }
 
     @Test
-    public void testGetPetDamage() {
-        System.out.println("getPetDamage");
-        SummonPet instance = new SummonPet();
-        int expResult = 0;
-        int result = instance.getPetDamage();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    public void testProperties() {
+        Spell spell = new SummonPet();
+        assertEquals(15, spell.getGoldCost());
+        assertEquals(30, spell.getmpCost());
+        assertEquals(6, spell.getMaxCooldown());
+        assertEquals("Summon Pet. Allows hero to summon a pet to his/her side. Cost 30 MP", spell.getMessage());
     }
 
     @Test
-    public void testGetPetHealth() {
-        System.out.println("getPetHealth");
-        SummonPet instance = new SummonPet();
-        int expResult = 0;
-        int result = instance.getPetHealth();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    public void buySpell() {
+        Spell spell = new SummonPet();
+        Hero hero = new Barbarian();
+        int initGold = hero.getGold();
+        assertTrue(hero.buy(spell));
+        int diff = initGold - hero.getGold();
+        assertEquals(spell.getGoldCost(), diff);
     }
 
     @Test
-    public void testSetPetDamage() {
-        System.out.println("setPetDamage");
-        int petDamage = 0;
-        SummonPet instance = new SummonPet();
-        instance.setPetDamage(petDamage);
-        fail("The test case is a prototype.");
+    public void testCastSpell() {
+        Spell spell = new SummonPet();
+        Hero caster = new Knight();
+        spell.castSpell(caster);
+        assertTrue(caster.getHasPet());
     }
-
-    @Test
-    public void testSetPetHealth() {
-        System.out.println("setPetHealth");
-        int petHealth = 0;
-        SummonPet instance = new SummonPet();
-        instance.setPetHealth(petHealth);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testRandomizeDamage() {
-        System.out.println("randomizeDamage");
-        SummonPet instance = new SummonPet();
-        int expResult = 0;
-        int result = instance.randomizeDamage();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testRandomizeHealth() {
-        System.out.println("randomizeHealth");
-        SummonPet instance = new SummonPet();
-        int expResult = 0;
-        int result = instance.randomizeHealth();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testCastSpell_Hero() {
-        System.out.println("castSpell");
-        Hero caster = null;
-        SummonPet instance = new SummonPet();
-        instance.castSpell(caster);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testCastSpell_Hero_Hero() {
-        System.out.println("castSpell");
-        Hero caster = null;
-        Hero victim = null;
-        SummonPet instance = new SummonPet();
-        instance.castSpell(caster, victim);
-        fail("The test case is a prototype.");
-    }
-    
 }
