275147a81baae86e6efd592d7460a18ed0ac4c8f
Shane Riggins <tuc33380@temple.edu>
2014-05-05 17:26:02 -0400

    Changing spaces to AttackSpace and RegularSpace

--- test/jftha/spaces/MonsterTest.java
+++ test/jftha/spaces/MonsterTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MonsterTest {
-    Space current;
+    RegularSpace current;
     Player hero;
     
     public MonsterTest() {
